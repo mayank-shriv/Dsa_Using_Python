3906da1b34b2f54e31ae01c2ebfd07ed0900532d
Mayank <shrivastavamayank01922@gmail.com>
2025-09-25 15:05:13 +0530

    updating code in maximumHeight.py file

--- TREE/maximumHeightBt.py
+++ TREE/maximumHeightBt.py
@@ -1,6 +1,4 @@
 # In this file we are creating code to find maximum depth (height ) of Binary Tree
-
-import math
 class Node:
     def __init__(self,data):
         self.data = data
@@ -26,3 +24,45 @@ def maximumHeight(node):
     rightHeight = maximumHeight(node.right)
     return 1+ max(leftHeight,rightHeight)
 print(maximumHeight(node1))
+
+
+
+
+
+# Iterative Method (Level Order)
+
+from collections import deque
+def level_order(root):
+    queue = deque([])
+    height = 0
+    queue.append(root)
+    while len(queue)!= 0:
+        level_size = len(queue)
+        height+=1
+        for _ in range(level_size):
+            e = queue.popleft()
+            if e.left is not None:
+                queue.append(e.left)
+            if e.right is not None:
+                queue.append(e.right)
+    return height
+print(level_order(node1))
+
+
+# myWay
+def levelOrder(node):
+    queue = []
+    height = 0
+    queue.append(node)
+    while  len(queue)!= 0:
+        levelSize = len(queue)
+        height += 1
+        for _ in range(levelSize):
+            e= queue[0]
+            del queue[0]
+            if e.left is not None:
+                queue.append(e.left)
+            if e.right is not None:
+                queue.append(e.right)
+    return height
+print(levelOrder(node1))
