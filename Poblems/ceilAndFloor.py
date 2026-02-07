5d9fa466c3317e80f416ea219dfe8fba3c69ae1a
Mayank <shrivastavamayank01922@gmail.com>
2026-02-07 08:05:10 +0530

    Add binary search implementation for finding floor and ceil values

---
+++ Poblems/ceilAndFloor.py
@@ -0,0 +1,31 @@
+nums = [2,3,4,4,5,6,7,8,9]
+target = 5
+n = len(nums)
+low = 0
+high = n-1
+ceil = -1 
+floor = -1
+
+while(low<=high):
+    mid = (low+high)//2
+
+    if nums[mid] == target:
+        print("index is:", mid)
+        floor = nums[mid]
+        ceil = nums[mid]
+        break
+    elif nums[mid]> target:
+        ciel = nums[mid]
+        high = mid-1
+        
+        
+
+    else:
+        floor = nums[mid]
+        low = mid+1
+print("floor is ", floor)
+print("ceil is ", ceil)
+        
+
+    
+
