87af47d05c72ec2b79d487484dd13f9ea60c8f4f
Mayank <shrivastavamayank01922@gmail.com>
2026-07-25 11:46:29 +0530

    Adding extra code for returning indices when it fount target

--- Infosys/03_return_indices_when_target_matched.py
+++ Infosys/03_return_indices_when_target_matched.py
@@ -14,23 +14,51 @@
 
 
 # Two sum problem 
+
+
+# Brute force
+
+# arr = list(map(int,input().split())) # This way we will take space separated values in a list 
+# target = int(input())
+# Here we are taking matching value
+
+# for i in range(len(arr)):
+#     j = i+1
+#     while (j<len(arr)):
+#         if arr[i]+arr[j] == target:
+#             print(*[i,j])
+#         j+=1
+
+# Optimal soln
+
 arr = list(map(int, input().split()))
 target = int(input())
+seen = {}
+for i in range(len(arr)):
+    value = target - arr[i]
+    if value in seen:
+        print(seen[value],i)
 
-left = 0
-right = len(arr) - 1
-found = False
+    seen[arr[i]] = i
+          
 
-while left < right:
-    current_sum = arr[left] + arr[right]
-    if current_sum == target:
-        print(left, right)
-        found = True
-        break
-    elif current_sum < target:
-        left += 1
-    else:
-        right -= 1
+# arr = list(map(int, input().split()))
+# target = int(input())
 
-if not found:
-    print("No pair found")
+# left = 0
+# right = len(arr) - 1
+# found = False
+
+# while left < right:
+#     current_sum = arr[left] + arr[right]
+#     if current_sum == target:
+#         print(left, right)
+#         found = True
+#         break
+#     elif current_sum < target:
+#         left += 1
+#     else:
+#         right -= 1
+
+# if not found:
+#     print("No pair found")
